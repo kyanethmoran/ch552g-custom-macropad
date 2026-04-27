d65f5821085e2b1973790cbd7950c991f5adb6c0
moran <sharon.ky.moran@gmail.com>
2026-04-27 17:58:47 -0400

    add required fields

--- desktop/app/validator.py
+++ desktop/app/validator.py
@@ -10,3 +10,15 @@ Catch problems such as:
 print warnings and skip broken action, avoid crashing
 """
 
+REQUIRED_FIELDS = {
+  "open_app": ["path"],
+  "open_url": ["url"],
+  "open_folder": ["path"],
+  "close_app": ["process_name"],
+  "send_hotkey": ["keys"],
+  "print_active_app": [],
+  "print_message": ["message"],
+  "toggle_mute_active_app": [],
+  "volume_up_active_app": [],
+  "volume_down_active_app": [],
+}
