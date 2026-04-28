1ad3d0428ddc35c70dbc770c0746318300fa1f85
moran <sharon.ky.moran@gmail.com>
2026-04-27 23:42:50 -0400

    save profile and dump into json

--- desktop/app/config.py
+++ desktop/app/config.py
@@ -9,3 +9,9 @@ def load_profile(profile_path: str):
   
   with path.open("r", encoding="utf=8") as file:
     return json.load(file)
+  
+def save_profile(profile_path: str, profile_data: dict):
+  path = Path(profile_path)
+
+  with path.open("w", encoding = "utf-8") as file:
+    json.dump(profile_data, file, indent=2)
